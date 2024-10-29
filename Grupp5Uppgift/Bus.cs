35532bfbf119159182e52be2ac55237b66a76edb
Svitlana <rsv0011@gmail.com>
2024-10-29 16:14:22 +0100

    fix: change the Bus methods

--- Grupp5Uppgift/Bus.cs
+++ Grupp5Uppgift/Bus.cs
@@ -9,11 +9,13 @@ namespace Grupp5Uppgift
         {
             public string Color { get; set; }
             public int Year { get; set; }
+             public string Model { get; set; }
 
-            public Bus(string color, int year)
-            {
+        public Bus(string color, int year, string model)
+            {,
                 Color = color;
                 Year = year;
+            Model = model;
             }
 
             public void DisplayCarInfo()
