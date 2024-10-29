008587a8a55595095dc31dd0a57bf1eb10f7092f
Dania Tamim <dania.tamim@live.se>
2024-10-29 14:09:51 +0100

    Add project files.

---
+++ Grupp5Uppgift/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Grupp5Uppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
