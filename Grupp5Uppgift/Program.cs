8ba3d6c208e8ce79e839d056921318f3308a8692
Svitlana <rsv0011@gmail.com>
2024-10-29 15:13:57 +0100

    fix: change the Car class to Bus

--- Grupp5Uppgift/Program.cs
+++ Grupp5Uppgift/Program.cs
@@ -7,23 +7,5 @@
             Console.WriteLine("Hello, World!");
         }
 
-        public class Car
-        {
-            public string Color { get; set; }
-            public int Year { get; set; }
-
-            public Car(string color, int year)
-            {
-                Color = color;
-                Year = year;
-            }
-
-            // Method
-            public void DisplayCarInfo()
-            {
-                Console.WriteLine($"Color: {Color}, Year: {Year}");
-            }
-        }
-
     }
 }
