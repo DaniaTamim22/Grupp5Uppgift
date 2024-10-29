b5992b63b3ce73105f831604344adc3f661f13dd
gayathrigeetha-thangavel <gayathrigeetha-thangavel@github.com>
2024-10-29 15:31:29 +0100

    Added my code in Program.cs

--- Grupp5Uppgift/Program.cs
+++ Grupp5Uppgift/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Gayathri gayathri = new Gayathri();
+            
         }
     }
 }
