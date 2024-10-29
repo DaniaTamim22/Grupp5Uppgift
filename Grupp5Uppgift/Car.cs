e43f8e1557b518d32573f4e4d540cbd4dc23f757
Dania Tamim <dania.tamim@live.se>
2024-10-29 15:03:15 +0100

    Add Car class

---
+++ Grupp5Uppgift/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp5Uppgift
+{
+    internal class Car
+    {
+        public string RegNum { get; private set; }
+        public int NumOfWheels { get; private set; }
+
+        public Car(string regNum,int numOfWheels)
+        {
+            RegNum= regNum.ToLower();
+            NumOfWheels = numOfWheels;
+        }
+
+    }
+}
