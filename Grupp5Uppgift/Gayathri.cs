b36e3e9e6b244aa94ac9b823ac54f82cd617603d
gayathrigeetha-thangavel <gayathrigeetha-thangavel@github.com>
2024-10-29 15:26:34 +0100

    Gayathri class has created

---
+++ Grupp5Uppgift/Gayathri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Grupp5Uppgift
+{
+    public class Gayathri
+    {
+
+        public Gayathri()
+        {
+            Console.WriteLine("Hello All");
+        }
+    }
+}
